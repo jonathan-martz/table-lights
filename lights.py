30d64afdf97a1e60a659ac79a7006d45be78ffd6
Jonathan Martz <github@jmartz.de>
2023-04-09 13:36:43 +0200

    Update Script for pocketbase

--- lights.py
+++ lights.py
@@ -12,15 +12,10 @@ led2 = Pin(2, Pin.OUT)
 timer = Timer()
 
 def blink(timer):
-    astronauts = urequests.get("https://tl.jmartz.gmbh/api/tisches/1").json()
-    if astronauts['data']['attributes']['reserviert'] == True:
+    astronauts = urequests.get("https://h4r1ng9nzv56lyu.pocketbase.dev/api/collections/table/records/owbamvr7bktsq38").json()
+    if astronauts['reserved'] == True:
         led1.high()
     else:
         led1.low()
-        
-    if astronauts['data']['attributes']['frei'] == True:
-        led2.high()
-    else:
-        led2.low()
-        
+                
 timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
